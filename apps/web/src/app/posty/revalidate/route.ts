6fb4c9f968dd11d98812fdf9267d9f23f45b7175
Oskar Puchalski <oskarpuchalski17@gmail.com>
2024-02-26 20:20:03 +0100

    feat: Chanched from GET to POST in api route

--- apps/web/src/app/posty/revalidate/route.ts
+++ apps/web/src/app/posty/revalidate/route.ts
@@ -1,6 +1,6 @@
 import { revalidateTag } from "next/cache";
 
-export async function GET() {
+export async function POST() {
  revalidateTag("articles");
  return Response.json({ revalidated: true, now: Date.now() });
 }
